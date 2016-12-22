dee0aa568ec3ad7a1aab935ca4a84c8c710cac4a
zstrad44 <zachstradling@gmail.com>
2016-12-22 14:39:48 -0700

    modified kele.rb, but still can't use IRB locally. Don't know why...

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,13 +1,20 @@
-require 'httparty'
+require "httparty"
 
-class kele
+class Kele
   include HTTParty
 
-  def initalize(email, password)
+  def initialize(email, password)
     @email = email
     @password = password
-    @base_url = 'https://www.bloc.io/api/v1/sessions'
-    @token = self.class.post(@base_url, body: { email: @email, password: @password })
+    @api_url = 'https://www.bloc.io/api/v1/sessions'
+    receive = self.class.post(@api_url + '/sessions', body: { email: @email, password: @password })
 
+      unless receive.code == 200
+
+           if receive.code == 404
+               raise "Email or password is invalid, please try   again"
+           end
+       end
+       @auth_token = receive["auth_token"]
   end
 end
