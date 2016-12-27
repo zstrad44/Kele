e84ed84e2b53a52119eb7934475a0ef647574540
zstrad44 <zachstradling@gmail.com>
2016-12-27 11:48:37 -0700

    completed checkpoint-3

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,24 @@
 require "httparty"
+require "json"
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
-    
+
     @api_url = 'https://www.bloc.io/api/v1'
     receive = self.class.post(@api_url + '/sessions', body: {  'email': email, 'password': password })
 
     raise ArgumentError, "Email or password is invalid, please try again" unless receive.code == 200
     @auth_token = receive["auth_token"]
   end
+
+  def get_me
+
+    receive = self.class.get(@api_url + '/users/me', headers: { "authorization" => @auth_token })
+
+    JSON.parse(receive.body)
+  end
+
+
 end
