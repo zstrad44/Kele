867f31028c8a06a1254edc5c2d737f21dc293591
zstrad44 <zachstradling@gmail.com>
2016-12-27 11:19:03 -0700

    completed checkpoint 2. Needed to install HTTParty locally

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,17 +4,11 @@ class Kele
   include HTTParty
 
   def initialize(email, password)
-    @email = email
-    @password = password
-    @api_url = 'https://www.bloc.io/api/v1/sessions'
-    receive = self.class.post(@api_url + '/sessions', body: { email: @email, password: @password })
+    
+    @api_url = 'https://www.bloc.io/api/v1'
+    receive = self.class.post(@api_url + '/sessions', body: {  'email': email, 'password': password })
 
-      unless receive.code == 200
-
-           if receive.code == 404
-               raise "Email or password is invalid, please try   again"
-           end
-       end
-       @auth_token = receive["auth_token"]
+    raise ArgumentError, "Email or password is invalid, please try again" unless receive.code == 200
+    @auth_token = receive["auth_token"]
   end
 end
