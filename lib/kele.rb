b3064a0ff4f54a807c83562f1cb1b36f0ab4e999
zstrad44 <zachstradling@gmail.com>
2016-12-30 15:06:00 -0700

    Possible completion of checkpoint-7. Not sure how to test.

--- lib/kele.rb
+++ lib/kele.rb
@@ -48,5 +48,16 @@ class Kele
          "token": token,
          "subject": subject,
          "stripped-text": body})
- end
+  end
+
+  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id,  comment, enrollment_id)
+    receive = self.class.post(@api_url + '/checkpoint_submissions',
+      headers: { "authorization" => @auth_token },
+       body: {
+         "assignment_branch": assignment_branch,
+         "assignment_commit_link": assignment_commit_link,
+         "checkpoint_id": checkpoint_id,
+         "comment": comment,
+         "enrollment_id": @enrollment_id })
+  end
 end
