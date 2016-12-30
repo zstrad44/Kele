8d5c238541abc81130d6c802851d8a0c6ede120a
zstrad44 <zachstradling@gmail.com>
2016-12-29 17:33:22 -0700

    completed checkpoint-6

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,7 +9,11 @@ class Kele
   def initialize(email, password)
 
     @api_url = 'https://www.bloc.io/api/v1'
-    receive = self.class.post(@api_url + '/sessions', body: {  'email': email, 'password': password })
+    receive = self.class.post(@api_url + '/sessions',
+      body: {
+        'email': email,
+        'password': password
+      })
 
     raise ArgumentError, "Email or password is invalid, please try again" unless receive.code == 200
     @auth_token = receive["auth_token"]
@@ -29,4 +33,20 @@ class Kele
     JSON.parse(receive.body).to_a
 
   end
+
+  def get_messages(page)
+    receive = self.class.get(@api_url + '/message_threads/', headers: { "authorization" => @auth_token }, body: {"page": page })
+    @messages = JSON.parse(receive.body)
+  end
+
+  def create_message(sender, recipient_id, token, subject, body)
+   receive = self.class.post(@api_url + '/messages',
+     headers: { "authorization" => @auth_token },
+       body: {
+         "sender": sender,
+         "recipient_id": recipient_id,
+         "token": token,
+         "subject": subject,
+         "stripped-text": body})
+ end
 end
